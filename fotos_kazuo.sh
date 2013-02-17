38d6fec13d8e328e50be2601cd6f9b889f9555fa
kazuoskk <kazuoskk@gmail.com>
2013-02-17 17:43:19 -0300

    correct return errors
    handle the return errors :-)

--- fotos_kazuo.sh
+++ fotos_kazuo.sh
@@ -22,35 +22,37 @@ echo "Log available at: $log";
 echo ;
 
 echo "sync ...";
-error=`rsync -rtuv $source $dest > $log/fotos_sync_$today.log`;
-if [ $error -ne 0 ];
+if ( rsync -rtuv $source $dest 2>&1 > $log/fotos_sync_$today.log );
 then
-    echo "rsync error..."
-    return -1;
-fi
+	echo "rsync ok";
+else
+	echo "rsync problem ... exiting";
+	return -1;
+fi 
 
 echo "executing diff...";
-error=`diff -r $source $dest > $log/fotos_diff_$today.log`;
-if [ $error -ne 0 ];
+if ( diff -r $source $dest 2>&1 > $log/fotos_diff_$today.log );
 then
-echo "diff error..."
-return -1;
+	echo "directories are equal";
+else	
+	echo "directories are differents";
 fi
 
 echo "open check file...";
-if  ! [ -s $log/fotos_diff_$today.log]
-    ! [ -f $log/fotos_diff_$today.log];
+if  ! [ -s $log/fotos_diff_$today.log ]
+    ! [ -f $log/fotos_diff_$today.log ];
 then
     echo "can not find the check file";
     return -1;
 fi;
 
 echo "checking diff...";
-error=`grep -c 'Only in $source' $log/fotos_diff_$today.log`;
-if [ $error -ne 0 ];
+if ( grep -c 'Only in $source' $log/fotos_diff_$today.log );
 then
     echo "somefiles can not copied to destination folder, please check the network and execute the command again.";
     return -1;
+else
+	echo "diff check is ok";
 fi
 
-echo "FINISHED DIRECTORIES SYNCED%n";
+echo "FINISHED DIRECTORIES SYNCED";
